27de5b7884760478f6dfb3054691e3e83debb2c8
jbbi <jb.bin35@gmail.com>
2015-09-06 21:15:56 +0200

    up

--- src/app/join/join.controller.js
+++ src/app/join/join.controller.js
@@ -3,17 +3,17 @@
 angular.module('regattaMobile')
 .controller('JoinCtrl', ['$scope', 'socket', function ($scope, socket) {
     $scope.games = [];
-		socket.on('connect', function() {
-			console.log('connected');
+	socket.on('connect', function() {
+		console.log('connected');
 
-			socket.emit('listgames', function(data) {
-				console.log(data);
-				$scope.games = data.data;
-			});
-
-			socket.on('listgames', function(data) {
-				console.log(data);
-				$scope.games = data.data;
-			});
+		socket.emit('listgames', function(data) {
+			console.log(data);
+			$scope.games = data.data;
 		});
+
+		socket.on('listgames', function(data) {
+			console.log(data);
+			$scope.games = data.data;
+		});
+	});
  }]);
