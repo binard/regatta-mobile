caf8ccf3cd9764a2511fe27513b4c04bdeb7f2e0
Thomas Nisole <thomasnisole@gmail.com>
2015-11-09 19:28:50 +0100

    put all cards in top

--- src/app/board/board.controller.js
+++ src/app/board/board.controller.js
@@ -17,14 +17,15 @@
       me.My = {
         cards: []
       };
+      me.My = {"playerName":"Thomas","boat":{"color":"#26A65B","number":"51","position":{"x":13,"y":31},"width":1,"length":5,"orientation":90},"started":true,"checkLines":[{"pointA":{"x":12,"y":30},"pointB":{"x":24,"y":30}},{"pointA":{"x":6,"y":7},"pointB":{"x":-200,"y":7}},{"pointA":{"x":6,"y":7},"pointB":{"x":6,"y":-200}},{"pointA":{"x":18,"y":18},"pointB":{"x":18,"y":200}},{"pointA":{"x":18,"y":18},"pointB":{"x":200,"y":18}},{"pointA":{"x":30,"y":7},"pointB":{"x":200,"y":7}},{"pointA":{"x":30,"y":7},"pointB":{"x":30,"y":200}},{"pointA":{"x":12,"y":30},"pointB":{"x":24,"y":30}}],"trappedTime":0,"cards":[{"name":"Triple babord","width":5,"height":7,"svgParams":{"card-trois-gauche":"display: block;"},"possibilities":[{"moves":[{"x":0,"y":3},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"2"},{"moves":[{"x":0,"y":5},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"4"},{"moves":[{"x":0,"y":7},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"6"}],"options":[],"type":"move","$$hashKey":"object:52"},{"name":"Tout droit nuage","width":5,"height":7,"svgParams":{"card-tout-droit":"display: block;","card-option-nuage":"display: block;"},"possibilities":[{"moves":[{"x":0,"y":8}],"rotation":0,"possibilityIndex":"9"}],"options":["cloud"],"type":"move","$$hashKey":"object:53"},{"name":"Tout droit","width":5,"height":7,"svgParams":{"card-tout-droit":"display: block;"},"possibilities":[{"moves":[{"x":0,"y":8}],"rotation":0,"possibilityIndex":"9"}],"options":[],"type":"move","$$hashKey":"object:54"},{"name":"Triple babord","width":5,"height":7,"svgParams":{"card-trois-gauche":"display: block;"},"possibilities":[{"moves":[{"x":0,"y":3},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"2"},{"moves":[{"x":0,"y":5},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"4"},{"moves":[{"x":0,"y":7},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"6"}],"options":[],"type":"move","$$hashKey":"object:55"},{"name":"Joker 1","width":5,"height":7,"svgParams":{"card-joker":"display: block;"},"possibilities":[{"moves":[{"x":0,"y":1},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"0"},{"moves":[{"x":0,"y":4},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"3"},{"moves":[{"x":0,"y":6},{"x":3,"y":0}],"rotation":-90,"possibilityIndex":"6"},{"moves":[{"x":0,"y":5},{"x":2,"y":0},{"x":0,"y":3}],"rotation":0,"possibilityIndex":"7"},{"moves":[{"x":0,"y":8}],"rotation":0,"possibilityIndex":"9"},{"moves":[{"x":0,"y":5},{"x":-2,"y":0},{"x":0,"y":3}],"rotation":0,"possibilityIndex":"11"},{"moves":[{"x":0,"y":6},{"x":-3,"y":0}],"rotation":90,"possibilityIndex":"13"},{"moves":[{"x":0,"y":4},{"x":-3,"y":0}],"rotation":90,"possibilityIndex":"15"},{"moves":[{"x":0,"y":1},{"x":-3,"y":0}],"rotation":90,"possibilityIndex":"18"}],"options":[],"type":"move","$$hashKey":"object:56"}],"gameId":"1"};
       me.mode = 'move';
 
-      socket.emit('my', function(response){
+      /*socket.emit('my', function(response){
         console.log('my', response);
         me.My = response.data.player;
         me.playerName = me.My.playerName;
         me.currentCardIndex = 0;
-      });
+      });*/
 
       this.startRemoteIsVisible = false;
       this.moveMapRemoteIsVisible = false;
@@ -32,6 +33,7 @@
       this.selectedCards = [];
       this.currentCardIndex = 0;
       this.removedCardsCount = 0;
+      this.previewNumber = 0;
 
       this.getCurrentImage = function(index){
         var paramStr = '';
@@ -51,9 +53,10 @@
 
       this.previewCard = function(numPossibilty){
         if(indexIsPossible(me.My.cards[me.currentCardIndex], numPossibilty)) {
-          console.log("previewCard card : " + me.My.cards[me.currentCardIndex]);
-          console.log("previewCard possbilityIndex : " + numPossibilty);
-          me.fnPreviewCard({index : me.currentCardIndex, card : me.My.cards[me.currentCardIndex], possibility : numPossibilty});
+          me.My.cards[me.currentCardIndex].preview = true;
+          me.My.cards[me.currentCardIndex].previewNumber = me.previewNumber;
+          me.previewNumber++;
+          console.log('coucou');
         }
         //this.pop();
       };
@@ -73,10 +76,6 @@
       this.showNextCard = function() {
         console.log("showNextCard");
         me.currentCardIndex = ++me.currentCardIndex % me.My.cards.length;
-      };
-
-      this.isVisible = function(index) {
-        return me.currentCardIndex === index;
       };
 
       this.showStartRemote = function() {
